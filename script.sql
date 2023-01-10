c41d20b0558dcee2c85581c8b43bfd4dda2c5193
Carlos <cortizmardones@gmail.com>
2023-01-09 22:51:52 -0300

    se modifica archivos script y se agrega FK a las tablas

--- script.sql
+++ script.sql
@@ -1,6 +1,13 @@
 
 ############ SCRIPT PARA GENERAR TABLAS ############ 
 
+CREATE TABLE `db_videogame_system`.`categoria` (
+  `id_categoria` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NULL,
+  `descripcion`  VARCHAR(255) NULL,
+  PRIMARY KEY (`id_categoria`),
+  UNIQUE INDEX `id_categoria_UNIQUE` (`id_categoria` ASC) VISIBLE);
+
 CREATE TABLE `db_videogame_system`.`videogame` (
   `id_videogame` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(255) NULL,
@@ -10,12 +17,7 @@ CREATE TABLE `db_videogame_system`.`videogame` (
   UNIQUE INDEX `id_videogame_UNIQUE` (`id_videogame` ASC) VISIBLE,
   foreign key (id_categoria) references categoria(id_Categoria) on delete cascade on update cascade);
 
-use db_videogame_system;
-
-select * from videogame;
-select * from categoria;
-
-ALTER TABLE categoria DROP COLUMN id;
+############ SCRIPT PARA CARGAR DATOS A LAS TABLAS ############ 
 
 insert into categoria values (null,"Aventura" , "");
 insert into categoria values (null,"Survival Horror" , "");
@@ -25,13 +27,19 @@ insert into videogame values (null,"Zelda: Breath Of The Wild" , 49999.9,1);
 insert into videogame values (null,"Animal Crossing: New Horizons" , 56999.9,1);
 insert into videogame values (null,"Red Dead Redemption 2" , 19990.9,1);
 
+
+############ QUERYS PARA OPERAR DATA ############ 
+
+use db_videogame_system;
+
+select * from videogame;
+select * from categoria;
+
+ALTER TABLE categoria DROP COLUMN id;
+
 DELETE FROM videogame WHERE id_videogame = 12;
 
 UPDATE videogame
 SET nombre ='Zelda: Breath Of The Wild' WHERE id_videogame = 1;
 
-## SQL (LENGUAJE ESTRUCTURADO DE CONSULTAS )
-
-use entregable3;
-select * from juego;
 
